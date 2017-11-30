7acd23f67defee13489891108292f581459ca2a2
Pauline <960707@live.se>
2017-11-30 23:19:27 +0100

    manage

--- reeds/admin/manage.php
+++ reeds/admin/manage.php
@@ -7,12 +7,25 @@
 
 				<h4>Add a book</h4>
 				<form>
+					<input type="text" name="bookID" placeholder="Book ID">
+					<input type="text" name="title" placeholder="Title">
+					<input type="author" name="author" placeholder="Author">
+					<input type="submit" name="add" value="add" class="submit">
 				</form>
 
 				<h4>Delete a book</h4>
 				<form>
 				</form>
 
+				<h3 style='font-family: roboto, sans-serf; color: orange; margin-bottom: 2px;'> Current Library </h3>
+				Press <a href='manage.php' style='color: purple; text-decoration: underline;'>here</a> to refresh.
+
+				<h4 style="margin-top: 10px;">Search</h4>
+					<form action="browse.php" method="POST">
+						<input type="text" name="searchtitle" placeholder="Title"><br>
+						<input type="text" name="searchauthor" placeholder="Author"><br>
+						<input type="submit" name="search" value="Search" class="submit">
+					</form>
 			<?php
 				$searchtitle = "";
 				$searchauthor = ""; 
@@ -64,13 +77,13 @@
 				$stmt = $db->prepare($query); 
 			    $stmt->bind_result($bookID, $title, $reserved, $authorID, $name); 
 			    $stmt->execute();
-			    echo "<h3 style='font-family: roboto, sans-serf; color: orange'> Current Library </h3>";
-			    echo "Press <a href='manage.php' style='color: purple; text-decoration: underline; '>here</a> to refresh";
+			    echo "";
+			    echo "";
 			    echo '<table>';
-			    echo '<tr><th>Title</th> <th>Author</th></tr>';
+			    echo '<tr><th>Book ID</th><th>Title</th><th>Author</th></tr>';
 			    while ($stmt->fetch()) {
 			        echo "<tr>";
-					echo "<td> $title </td><td> $name </td>";
+					echo "<td style='text-align:right; padding-right: 28px; font-weight: bold; '>$bookID</td><td> $title </td><td> $name </td>";
 				    echo "</tr>";   
 			    }
 			    echo "</table>";
